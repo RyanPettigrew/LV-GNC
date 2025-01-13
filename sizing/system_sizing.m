9a76490a1c21999c028b16c760c48cbefecb30ff
apresto <prestoa9@gmail.com>
2025-01-13 13:20:05 -0800

    Create system_sizing.m

---
+++ sizing/system_sizing.m
@@ -0,0 +1,11 @@
+function system_sizing()
+    % =====================================================================
+    % THIS FUNCTION SERVES TO PROVIDE SIZING APPROXIMATIONS
+    % FOR LAUNCH VEHICLES GIVEN BASIC PARAMETERS
+    % =====================================================================
+
+    clc; close all;
+    % ---------------------------------------------------------------------
+    
+
+end
